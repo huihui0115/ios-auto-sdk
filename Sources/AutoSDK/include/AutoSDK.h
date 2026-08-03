0037958dff2ff7386e3d485489e17a0e67a1bb56
huihui0115 <312540637+huihui0115@users.noreply.github.com>
2026-08-03 21:55:10 +0800

    Fix umbrella header nullability scope

--- Sources/AutoSDK/include/AutoSDK.h
+++ Sources/AutoSDK/include/AutoSDK.h
@@ -1,15 +1,14 @@
 #import <Foundation/Foundation.h>
-
-NS_ASSUME_NONNULL_BEGIN
-
-FOUNDATION_EXPORT double AutoSDKVersionNumber;
-FOUNDATION_EXPORT const unsigned char AutoSDKVersionString[];
-
 #import "AutoEngine.h"
 #import "AutoAutomationAdapter.h"
 #import "AutoUIKitAdapter.h"
 #import "AutoWDAHTTPAdapter.h"
 #import "AutoDebugServer.h"
 #import "AutoSDKError.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+FOUNDATION_EXPORT double AutoSDKVersionNumber;
+FOUNDATION_EXPORT const unsigned char AutoSDKVersionString[];
 
 NS_ASSUME_NONNULL_END
